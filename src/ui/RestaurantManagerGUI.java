269dd9fd6a6b7c085b72f3403b9c6c50d6a4cb78
KennetSanchez <kesasaro@gmail.com>
2021-03-16 17:15:11 -0500

    GUI bases
    The menu-bar has the buttons that we are going to need. The first view has the login so the user can (or not) access to the options. Also the class "RestaurantManagerGUI" has some code with the declaration of some items.

---
+++ src/ui/RestaurantManagerGUI.java
@@ -0,0 +1,64 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class RestaurantManagerGUI {
+
+	// Mainpane items.
+
+	@FXML
+	private Label companyLogo;
+
+	@FXML
+	private Label date;
+
+	@FXML
+	private Pane mainPane;
+
+	@FXML
+	private TextField userText;
+
+	@FXML
+	private PasswordField passwordText;
+
+	@FXML
+	void gestionateClients(ActionEvent event) {
+
+	}
+
+	@FXML
+	void gestionateEmployees(ActionEvent event) {
+
+	}
+
+	@FXML
+	void gestionateProducts(ActionEvent event) {
+
+	}
+
+	@FXML
+	void gestionateUsers(ActionEvent event) {
+
+	}
+
+	@FXML
+	void login(ActionEvent event) {
+
+	}
+
+	@FXML
+	void showDevelopers(ActionEvent event) {
+
+	}
+
+	@FXML
+	void showHelp(ActionEvent event) {
+
+	}
+
+}
