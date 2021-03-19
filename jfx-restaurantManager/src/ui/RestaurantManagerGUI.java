6107663754640be13d40a277cf0d8826bec6cca6
KennetSanchez <kesasaro@gmail.com>
2021-03-19 16:16:28 -0500

    GUI advance
    We create the GUI for the ingredients, and meal windows. Also declared the items on RestaurantManagerGUI. Also change the name of "createMeal.fxml" to "CreateMeal.fxml".

--- jfx-restaurantManager/src/ui/RestaurantManagerGUI.java
+++ jfx-restaurantManager/src/ui/RestaurantManagerGUI.java
@@ -2,24 +2,26 @@ package ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import java.time.LocalDateTime;
 
-
-
 public class RestaurantManagerGUI {
 
-	//It's not sure that this keeps actualizated.
+	// It's not sure that this keeps actualizated.
 	public String timeAndDate() {
 		LocalDateTime ldt = LocalDateTime.now();
 		String msg = ldt + "";
 		return msg;
 	}
-	
-	//Mainpane items.
+
+	// Mainpane items.
 
 	@FXML
 	private Label companyLogo;
@@ -35,13 +37,7 @@ public class RestaurantManagerGUI {
 
 	@FXML
 	private PasswordField passwordText;
-	
-	//Ingredients code.
-	
-	@FXML
-    private Pane ingredientsPane;
-	
-	
+
 	//Menu code.
 	@FXML
 	void gestionateClients(ActionEvent event) {
@@ -75,6 +71,63 @@ public class RestaurantManagerGUI {
 
 	@FXML
 	void showHelp(ActionEvent event) {
+
+	}
+
+	// Ingredients code.
+
+	@FXML
+    private Pane ingredientsPane;
+
+    @FXML
+    private TextField txtIngredient;
+
+    @FXML
+    private CheckBox allergenCheckBox;
+
+    @FXML
+    private TableView<?> tvIngredientsCreated;
+
+    @FXML
+    private TableColumn<?, ?> tcIngredientsCreated;
+
+
+	//Create meal code.
+	@FXML
+	private TableView<?> tvMeal;
+
+	@FXML
+	private TableColumn<?, ?> tcMeal;
+
+	@FXML
+	private TableView<?> tvType;
+
+	@FXML
+	private TableColumn<?, ?> tcType;
+
+	@FXML
+	private TextField txtMeal;
+
+	@FXML
+	private TextField txtType;
+
+	@FXML
+	private TextField txtPrice;
+
+	@FXML
+	private TextField txtSize;
+
+	@FXML
+	private TextArea areaIngredients;
+
+	@FXML
+	private TableView<?> tvIngredients;
+
+	@FXML
+	private TableColumn<?, ?> tcIngredients;
+
+	@FXML
+	void createMeal(ActionEvent event) {
 
 	}
 
